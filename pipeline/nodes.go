fe12121ec91d10466dd559f4c7623aa45a7ea1b7
ryang <ryang@evolveconsulting.com.hk>
2019-02-03 19:53:52 +0800

    init

---
+++ pipeline/nodes.go
@@ -0,0 +1,107 @@
+package pipeline
+
+import (
+	"encoding/binary"
+	"io"
+	"math/rand"
+	"sort"
+)
+
+func ArraySource(a ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, v := range a {
+			out <- v
+		}
+		close(out)
+	}()
+	return out
+}
+
+func InMemSort(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		a := []int{}
+		// Read into memory
+		for v := range in {
+			a = append(a, v)
+		}
+
+		// Sort
+		sort.Ints(a)
+
+		// Output
+		for _, v := range a {
+			out <- v
+		}
+		close(out)
+	}()
+	return out
+}
+
+func Merge(in1, in2 <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		v1, ok1 := <-in1
+		v2, ok2 := <-in2
+		for ok1 || ok2 {
+			if !ok2 || (ok1 && v1 <= v2) {
+				out <- v1
+				v1, ok1 = <-in1
+			} else {
+				out <- v2
+				v2, ok2 = <-in2
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func ReaderSource(reader io.Reader, chunkSize int) <-chan int {
+	out := make(chan int)
+	go func() {
+		buffer := make([]byte, 8)
+		bytesRead := 0
+		for {
+			n, err := reader.Read(buffer)
+			bytesRead += n
+			if n > 0 {
+				v := int(binary.BigEndian.Uint64(buffer))
+				out <- v
+			}
+			if err != nil || (chunkSize != -1 && bytesRead >= chunkSize) {
+				break
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func WirterSink(writer io.Writer, in <-chan int) {
+	for v := range in {
+		buffer := make([]byte, 8)
+		binary.BigEndian.PutUint64(buffer, uint64(v))
+		writer.Write(buffer)
+	}
+}
+
+func RandomSource(count int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < count; i++ {
+			out <- rand.Int()
+		}
+		close(out)
+	}()
+	return out
+}
+
+func MergeN(inputs ...<-chan int) <-chan int {
+	if len(inputs) == 1 {
+		return inputs[0]
+	}
+	m := len(inputs) / 2
+	return Merge(MergeN(inputs[:m]...), MergeN(inputs[m:]...))
+}
